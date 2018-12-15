3565316b2559f768f14e1f2ff6d6c686a4ae10f1
Linus Yang <laokongzi@gmail.com>
2018-12-15 19:31:30 +0800

    Use chacha for arc4random

--- tools/src/arc4random.c
+++ tools/src/arc4random.c
@@ -1,15 +1,11 @@
-/* Portable arc4random.c based on arc4random.c from OpenBSD.
- * Portable version by Chris Davis, adapted for Libevent by Nick Mathewson
- * Copyright (c) 2010 Chris Davis, Niels Provos, and Nick Mathewson
- * Copyright (c) 2010-2012 Niels Provos and Nick Mathewson
- *
- * Note that in Libevent, this file isn't compiled directly.  Instead,
- * it's included from evutil_rand.c
- */
+/*	$OpenBSD: arc4random.c,v 1.53 2015/09/10 18:53:50 bcook Exp $	*/
 
 /*
  * Copyright (c) 1996, David Mazieres <dm@uun.org>
  * Copyright (c) 2008, Damien Miller <djm@openbsd.org>
+ * Copyright (c) 2013, Markus Friedl <markus@openbsd.org>
+ * Copyright (c) 2014, Theo de Raadt <deraadt@openbsd.org>
+ * Copyright (c) 2015, Guillem Jover <guillem@hadrons.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -25,390 +21,290 @@
  */
 
 /*
- * Arc4 random number generator for OpenBSD.
- *
- * This code is derived from section 17.1 of Applied Cryptography,
- * second edition, which describes a stream cipher allegedly
- * compatible with RSA Labs "RC4" cipher (the actual description of
- * which is a trade secret).  The same algorithm is used as a stream
- * cipher called "arcfour" in Tatu Ylonen's ssh package.
- *
- * Here the stream cipher has been modified always to include the time
- * when initializing the state.  That makes it impossible to
- * regenerate the same random sequence twice, so this can't be used
- * for encryption, but will generate good random numbers.
- *
- * RC4 is a registered trademark of RSA Laboratories.
+ * ChaCha based random number generator for OpenBSD.
  */
 
-#ifndef ARC4RANDOM_EXPORT
-#define ARC4RANDOM_EXPORT
-#endif
-
-#ifndef ARC4RANDOM_UINT32
-#define ARC4RANDOM_UINT32 uint32_t
-#endif
-
-#define ARC4_LOCK_()
-#define ARC4_UNLOCK_()
-
-#ifndef ARC4RANDOM_NO_INCLUDES
-#ifdef _WIN32
-#include <windows.h>
-#include <wincrypt.h>
-#include <process.h>
-#else
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/param.h>
-#include <sys/time.h>
-#include <errno.h>
-#endif
-#endif
 #include <stdint.h>
-#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 
-/* Add platform entropy 32 bytes (256 bits) at a time. */
-#define ADD_ENTROPY 32
+#define KEYSTREAM_ONLY
+#include "chacha_private.h"
 
-/* Re-seed from the platform RNG after generating this many bytes. */
-#define BYTES_BEFORE_RESEED 1600000
+#define minimum(a, b) ((a) < (b) ? (a) : (b))
 
-struct arc4_stream {
-	unsigned char i;
-	unsigned char j;
-	unsigned char s[256];
-};
+#if defined(__GNUC__) || defined(__clang__) || defined(_MSC_VER)
+#define inline __inline
+#else				/* __GNUC__ || __clang__ || _MSC_VER */
+#define inline
+#endif				/* !__GNUC__ && !__clang__ && !_MSC_VER */
 
-#ifdef _WIN32
-#define getpid _getpid
-#define pid_t int
-#endif
+#define KEYSZ	32
+#define IVSZ	8
+#define BLOCKSZ	64
+#define RSBUFSZ	(16*BLOCKSZ)
 
-static int rs_initialized;
-static struct arc4_stream rs;
-static pid_t arc4_stir_pid;
-static int arc4_count;
-static int arc4_seeded_ok;
+/* Marked MAP_INHERIT_ZERO, so zero'd out in fork children. */
+typedef struct _rs {
+	size_t		rs_have;	/* valid bytes at end of rs_buf */
+	size_t		rs_count;	/* bytes till reseed */
+} rs_t;
 
-static inline unsigned char arc4_getbyte(void);
+/* Maybe be preserved in fork children, if _rs_allocate() decides. */
+typedef struct _rsx {
+	chacha_ctx	rs_chacha;	/* chacha context for random keystream */
+	unsigned char	rs_buf[RSBUFSZ];	/* keystream blocks */
+} rsx_t;
 
-static inline void
-arc4_init(void)
-{
-	int     n;
+static rs_t rs_s = {0};
+static rsx_t rsx_s = {0};
+static rs_t *rs = NULL;
+static rsx_t *rsx = NULL;
 
-	for (n = 0; n < 256; n++)
-		rs.s[n] = n;
-	rs.i = 0;
-	rs.j = 0;
-}
-
-static inline void
-arc4_addrandom(const unsigned char *dat, int datlen)
-{
-	int     n;
-	unsigned char si;
-
-	rs.i--;
-	for (n = 0; n < 256; n++) {
-		rs.i = (rs.i + 1);
-		si = rs.s[rs.i];
-		rs.j = (rs.j + si + dat[n % datlen]);
-		rs.s[rs.i] = rs.s[rs.j];
-		rs.s[rs.j] = si;
-	}
-	rs.j = rs.i;
-}
-
-/**
- * Volatile pointer to memset: we use this to keep the compiler from
- * eliminating our call to memset.
- */
-void * (*volatile evutil_memset_volatile_)(void *, int, size_t) = memset;
-
-void
-evutil_memclear_(void *mem, size_t len)
-{
-	evutil_memset_volatile_(mem, 0, len);
-}
-
-#ifndef _WIN32
-static ssize_t
-read_all(int fd, unsigned char *buf, size_t count)
-{
-	size_t numread = 0;
-	ssize_t result;
-
-	while (numread < count) {
-		result = read(fd, buf+numread, count-numread);
-		if (result<0)
-			return -1;
-		else if (result == 0)
-			break;
-		numread += result;
-	}
-
-	return (ssize_t)numread;
-}
-#endif
-
-#ifdef _WIN32
-#define TRY_SEED_WIN32
-static int
-arc4_seed_win32(void)
-{
-	/* This is adapted from Tor's crypto_seed_rng() */
-	static int provider_set = 0;
-	static HCRYPTPROV provider;
-	unsigned char buf[ADD_ENTROPY];
-
-	if (!provider_set) {
-		if (!CryptAcquireContext(&provider, NULL, NULL, PROV_RSA_FULL,
-		    CRYPT_VERIFYCONTEXT)) {
-			if (GetLastError() != (DWORD)NTE_BAD_KEYSET)
-				return -1;
-		}
-		provider_set = 1;
-	}
-	if (!CryptGenRandom(provider, sizeof(buf), buf))
-		return -1;
-	arc4_addrandom(buf, sizeof(buf));
-	evutil_memclear_(buf, sizeof(buf));
-	arc4_seeded_ok = 1;
-	return 0;
-}
-#endif
-
-#ifndef _WIN32
-#define TRY_SEED_URANDOM
-static char *arc4random_urandom_filename = NULL;
-
-int
-evutil_open_closeonexec_(const char *pathname, int flags, unsigned mode)
-{
-	int fd;
-
-#ifdef O_CLOEXEC
-	fd = open(pathname, flags|O_CLOEXEC, (mode_t)mode);
-	if (fd >= 0 || errno == EINVAL)
-		return fd;
-	/* If we got an EINVAL, fall through and try without O_CLOEXEC */
-#endif
-	fd = open(pathname, flags, (mode_t)mode);
-	if (fd < 0)
-		return -1;
-
-#if defined(FD_CLOEXEC)
-	if (fcntl(fd, F_SETFD, FD_CLOEXEC) < 0) {
-		close(fd);
-		return -1;
-	}
-#endif
-
-	return fd;
-}
-
-static int arc4_seed_urandom_helper_(const char *fname)
-{
-	unsigned char buf[ADD_ENTROPY];
-	int fd;
-	size_t n;
-
-	fd = evutil_open_closeonexec_(fname, O_RDONLY, 0);
-	if (fd<0)
-		return -1;
-	n = read_all(fd, buf, sizeof(buf));
-	close(fd);
-	if (n != sizeof(buf))
-		return -1;
-	arc4_addrandom(buf, sizeof(buf));
-	evutil_memclear_(buf, sizeof(buf));
-	arc4_seeded_ok = 1;
-	return 0;
-}
-
-static int
-arc4_seed_urandom(void)
-{
-	/* This is adapted from Tor's crypto_seed_rng() */
-	static const char *filenames[] = {
-		"/dev/srandom", "/dev/urandom", "/dev/random", NULL
-	};
-	int i;
-	if (arc4random_urandom_filename)
-		return arc4_seed_urandom_helper_(arc4random_urandom_filename);
-
-	for (i = 0; filenames[i]; ++i) {
-		if (arc4_seed_urandom_helper_(filenames[i]) == 0) {
-			return 0;
-		}
-	}
-
-	return -1;
-}
-#endif
-
-static int
-arc4_seed(void)
-{
-	int ok = 0;
-	/* We try every method that might work, and don't give up even if one
-	 * does seem to work.  There's no real harm in over-seeding, and if
-	 * one of these sources turns out to be broken, that would be bad. */
-#ifdef TRY_SEED_WIN32
-	if (0 == arc4_seed_win32())
-		ok = 1;
-#endif
-#ifdef TRY_SEED_URANDOM
-	if (0 == arc4_seed_urandom())
-		ok = 1;
-#endif
-	return ok ? 0 : -1;
-}
-
-static int
-arc4_stir(void)
-{
-	int     i;
-
-	if (!rs_initialized) {
-		arc4_init();
-		rs_initialized = 1;
-	}
-
-	arc4_seed();
-	if (!arc4_seeded_ok)
-		return -1;
-
-	/*
-	 * Discard early keystream, as per recommendations in
-	 * "Weaknesses in the Key Scheduling Algorithm of RC4" by
-	 * Scott Fluhrer, Itsik Mantin, and Adi Shamir.
-	 * http://www.wisdom.weizmann.ac.il/~itsik/RC4/Papers/Rc4_ksa.ps
-	 *
-	 * Ilya Mironov's "(Not So) Random Shuffles of RC4" suggests that
-	 * we drop at least 2*256 bytes, with 12*256 as a conservative
-	 * value.
-	 *
-	 * RFC4345 says to drop 6*256.
-	 *
-	 * At least some versions of this code drop 4*256, in a mistaken
-	 * belief that "words" in the Fluhrer/Mantin/Shamir paper refers
-	 * to processor words.
-	 *
-	 * We add another sect to the cargo cult, and choose 12*256.
-	 */
-	for (i = 0; i < 12*256; i++)
-		(void)arc4_getbyte();
-
-	arc4_count = BYTES_BEFORE_RESEED;
-
-	return 0;
-}
-
+#define _ARC4_LOCK()
+#define _ARC4_UNLOCK()
+#define _rs_forkdetect()
 
 static void
-arc4_stir_if_needed(void)
-{
-	pid_t pid = getpid();
+explicit_bzero(void *v, size_t n) {
+    volatile unsigned char *p = v; while( n-- ) *p++ = 0;
+}
 
-	if (arc4_count <= 0 || !rs_initialized || arc4_stir_pid != pid)
-	{
-		arc4_stir_pid = pid;
-		arc4_stir();
+#if defined(__linux__)
+
+#include <sys/syscall.h>
+
+#ifdef SYS_getrandom
+
+#ifndef GRND_NONBLOCK
+#define GRND_NONBLOCK   0x0001
+#endif
+
+#endif
+
+static int
+getentropy(void *buf, size_t len)
+{
+	int ret;
+	if (len > 256)
+		return (-1);
+	ret = syscall(SYS_getrandom, buf, len, GRND_NONBLOCK);
+	if (ret != (int)len)
+		return (-1);
+	return (0);
+}
+
+#elif defined(_WIN32)
+
+#include <windows.h>
+#include <wincrypt.h>
+
+static int
+getentropy(void *buf, size_t len)
+{
+	HCRYPTPROV provider;
+	if (len > 256) {
+		return (-1);
+	}
+	if (CryptAcquireContext(&provider, NULL, NULL, PROV_RSA_FULL,
+	    CRYPT_VERIFYCONTEXT) == 0)
+		goto fail;
+	if (CryptGenRandom(provider, len, buf) == 0) {
+		CryptReleaseContext(provider, 0);
+		goto fail;
+	}
+	CryptReleaseContext(provider, 0);
+	return (0);
+fail:
+	return (-1);
+}
+
+#endif
+
+static int
+getentropy_fallback(uint8_t *output, int len) {
+    static uint64_t s[2] = {0};
+    static int inited = 0;
+    if (!inited) {
+        uint32_t seed = (uint32_t)time(NULL);
+        s[0] = seed | 0x100000000L;
+        s[1] = ((uint64_t)seed << 32) | 0x1;
+        inited = 1;
+    }
+
+    while (len > 0) {
+        uint32_t rd;
+        const int blen = minimum(len, sizeof(rd));
+        uint64_t *state = &s[0];
+        uint64_t *inc = &s[1];
+
+        // *Really* minimal PCG32 code / (c) 2014 M.E. O'Neill / pcg-random.org
+        // Licensed under Apache License 2.0 (NO WARRANTY, etc. see website)
+        uint64_t oldstate = *state;
+        // Advance internal state
+        *state = oldstate * 6364136223846793005ULL + (*inc|1);
+        // Calculate output function (XSH RR), uses old state for max ILP
+        uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+        uint32_t rot = oldstate >> 59u;
+        rd = (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+
+        memcpy(output, (uint8_t *)&rd, blen);
+        output += blen;
+        len    -= blen;
+    }
+    return 1;
+}
+
+static inline void
+_rs_init(unsigned char *buf, size_t n)
+{
+	if (n < KEYSZ + IVSZ)
+		return;
+
+	if (rs == NULL) {
+		rs = &rs_s;
+		rsx = &rsx_s;
+		explicit_bzero(rs, sizeof(rs_s));
+		explicit_bzero(rsx, sizeof(rsx_s));
+	}
+
+	chacha_keysetup(&rsx->rs_chacha, buf, KEYSZ * 8, 0);
+	chacha_ivsetup(&rsx->rs_chacha, buf + KEYSZ);
+}
+
+static inline void
+_rs_rekey(unsigned char *dat, size_t datlen)
+{
+#ifndef KEYSTREAM_ONLY
+	memset(rsx->rs_buf, 0, sizeof(rsx->rs_buf));
+#endif
+	/* fill rs_buf with the keystream */
+	chacha_encrypt_bytes(&rsx->rs_chacha, rsx->rs_buf,
+	    rsx->rs_buf, sizeof(rsx->rs_buf));
+	/* mix in optional user provided data */
+	if (dat) {
+		size_t i, m;
+
+		m = minimum(datlen, KEYSZ + IVSZ);
+		for (i = 0; i < m; i++)
+			rsx->rs_buf[i] ^= dat[i];
+	}
+	/* immediately reinit for backtracking resistance */
+	_rs_init(rsx->rs_buf, KEYSZ + IVSZ);
+	memset(rsx->rs_buf, 0, KEYSZ + IVSZ);
+	rs->rs_have = sizeof(rsx->rs_buf) - KEYSZ - IVSZ;
+}
+
+static void
+_rs_stir(void)
+{
+	unsigned char rnd[KEYSZ + IVSZ];
+
+	if (getentropy(rnd, sizeof rnd) == -1) {
+		getentropy_fallback(rnd, sizeof rnd);
+	}
+
+	if (!rs)
+		_rs_init(rnd, sizeof(rnd));
+	else
+		_rs_rekey(rnd, sizeof(rnd));
+	explicit_bzero(rnd, sizeof(rnd));	/* discard source seed */
+
+	/* invalidate rs_buf */
+	rs->rs_have = 0;
+	memset(rsx->rs_buf, 0, sizeof(rsx->rs_buf));
+
+	rs->rs_count = 1600000;
+}
+
+static inline void
+_rs_stir_if_needed(size_t len)
+{
+	_rs_forkdetect();
+	if (!rs || rs->rs_count <= len)
+		_rs_stir();
+	if (rs->rs_count <= len)
+		rs->rs_count = 0;
+	else
+		rs->rs_count -= len;
+}
+
+static inline void
+_rs_random_buf(void *_buf, size_t n)
+{
+	unsigned char *buf = (unsigned char *)_buf;
+	unsigned char *keystream;
+	size_t m;
+
+	_rs_stir_if_needed(n);
+	while (n > 0) {
+		if (rs->rs_have > 0) {
+			m = minimum(n, rs->rs_have);
+			keystream = rsx->rs_buf + sizeof(rsx->rs_buf)
+			    - rs->rs_have;
+			memcpy(buf, keystream, m);
+			memset(keystream, 0, m);
+			buf += m;
+			n -= m;
+			rs->rs_have -= m;
+		}
+		if (rs->rs_have == 0)
+			_rs_rekey(NULL, 0);
 	}
 }
 
-static inline unsigned char
-arc4_getbyte(void)
+static inline void
+_rs_random_u32(uint32_t *val)
 {
-	unsigned char si, sj;
+	unsigned char *keystream;
 
-	rs.i = (rs.i + 1);
-	si = rs.s[rs.i];
-	rs.j = (rs.j + si);
-	sj = rs.s[rs.j];
-	rs.s[rs.i] = sj;
-	rs.s[rs.j] = si;
-	return (rs.s[(si + sj) & 0xff]);
+	_rs_stir_if_needed(sizeof(*val));
+	if (rs->rs_have < sizeof(*val))
+		_rs_rekey(NULL, 0);
+	keystream = rsx->rs_buf + sizeof(rsx->rs_buf) - rs->rs_have;
+	memcpy(val, keystream, sizeof(*val));
+	memset(keystream, 0, sizeof(*val));
+	rs->rs_have -= sizeof(*val);
 }
 
-static inline unsigned int
-arc4_getword(void)
-{
-	unsigned int val;
-
-	val = arc4_getbyte() << 24;
-	val |= arc4_getbyte() << 16;
-	val |= arc4_getbyte() << 8;
-	val |= arc4_getbyte();
-
-	return val;
-}
-
-#ifndef ARC4RANDOM_NOSTIR
-ARC4RANDOM_EXPORT int
+void
 arc4random_stir(void)
 {
-	int val;
-	ARC4_LOCK_();
-	val = arc4_stir();
-	ARC4_UNLOCK_();
-	return val;
+	_ARC4_LOCK();
+	_rs_stir();
+	_ARC4_UNLOCK();
 }
-#endif
 
-#ifndef ARC4RANDOM_NOADDRANDOM
-ARC4RANDOM_EXPORT void
-arc4random_addrandom(const unsigned char *dat, int datlen)
+void
+arc4random_addrandom(unsigned char *dat, int datlen)
 {
-	int j;
-	ARC4_LOCK_();
-	if (!rs_initialized)
-		arc4_stir();
-	for (j = 0; j < datlen; j += 256) {
-		/* arc4_addrandom() ignores all but the first 256 bytes of
-		 * its input.  We want to make sure to look at ALL the
-		 * data in 'dat', just in case the user is doing something
-		 * crazy like passing us all the files in /var/log. */
-		arc4_addrandom(dat + j, datlen - j);
-	}
-	ARC4_UNLOCK_();
+	_ARC4_LOCK();
+	_rs_stir_if_needed(datlen);
+	_rs_rekey(dat, datlen);
+	_ARC4_UNLOCK();
 }
-#endif
 
-#ifndef ARC4RANDOM_NORANDOM
-ARC4RANDOM_EXPORT ARC4RANDOM_UINT32
+uint32_t
 arc4random(void)
 {
-	ARC4RANDOM_UINT32 val;
-	ARC4_LOCK_();
-	arc4_count -= 4;
-	arc4_stir_if_needed();
-	val = arc4_getword();
-	ARC4_UNLOCK_();
+	uint32_t val;
+
+	_ARC4_LOCK();
+	_rs_random_u32(&val);
+	_ARC4_UNLOCK();
 	return val;
 }
-#endif
 
-ARC4RANDOM_EXPORT void
-arc4random_buf(void *buf_, size_t n)
+void
+arc4random_buf(void *buf, size_t n)
 {
-	unsigned char *buf = buf_;
-	ARC4_LOCK_();
-	arc4_stir_if_needed();
-	while (n--) {
-		if (--arc4_count <= 0)
-			arc4_stir();
-		buf[n] = arc4_getbyte();
-	}
-	ARC4_UNLOCK_();
+	_ARC4_LOCK();
+	_rs_random_buf(buf, n);
+	_ARC4_UNLOCK();
 }
 
-#ifndef ARC4RANDOM_NOUNIFORM
 /*
  * Calculate a uniformly distributed random number less than upper_bound
  * avoiding "modulo bias".
@@ -419,25 +315,16 @@ arc4random_buf(void *buf_, size_t n)
  * [2**32 % upper_bound, 2**32) which maps back to [0, upper_bound)
  * after reduction modulo upper_bound.
  */
-ARC4RANDOM_EXPORT unsigned int
-arc4random_uniform(unsigned int upper_bound)
+uint32_t
+arc4random_uniform(uint32_t upper_bound)
 {
-	ARC4RANDOM_UINT32 r, min;
+	uint32_t r, min;
 
 	if (upper_bound < 2)
 		return 0;
 
-#if (UINT_MAX > 0xffffffffUL)
-	min = 0x100000000UL % upper_bound;
-#else
-	/* Calculate (2**32 % upper_bound) avoiding 64-bit math */
-	if (upper_bound > 0x80000000)
-		min = 1 + ~upper_bound;		/* 2**32 - upper_bound */
-	else {
-		/* (2**32 - (x * 2)) % x == 2**32 % x when x <= 2**31 */
-		min = ((0xffffffff - (upper_bound * 2)) + 1) % upper_bound;
-	}
-#endif
+	/* 2**32 % x == (2**32 - x) % x */
+	min = -upper_bound % upper_bound;
 
 	/*
 	 * This could theoretically loop forever but each retry has
@@ -453,4 +340,3 @@ arc4random_uniform(unsigned int upper_bound)
 
 	return r % upper_bound;
 }
-#endif
